50b38a759df01adb280ca48ecc2e71317ace796d
BAHATTIN KOC <bahattin.koc@atmosware.turkcell.com.tr>
2024-05-21 01:28:38 +0300

    - Project created

---
+++ carrem/carremApp.swift
@@ -0,0 +1,18 @@
+//
+//  carremApp.swift
+//  carrem
+//
+//  Created by BAHATTIN KOC on 21.05.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct carremApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
